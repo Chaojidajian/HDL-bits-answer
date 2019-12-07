74c58171192a404d284fd526782b55e3846e364d
Bingjian Du <43827518+Chaojidajian@users.noreply.github.com>
2019-12-07 15:04:59 -0500

    Create Rule110.v

---
+++ Rule110.v
@@ -0,0 +1,21 @@
+module top_module(
+    input clk,
+    input load,
+    input [511:0] data,
+    output [511:0] q ); 
+    integer i;
+    always@(posedge clk) begin
+        if(load)
+            q<=data;
+        else begin
+            for(i=1;i<511;i=i+1)begin
+                q[i]<=(q[i]||q[i-1])&&(~q[i]||~q[i-1]||~q[i+1]);
+            end
+            q[0]<=q[0];
+            q[511]<=q[510]|q[511];
+        end
+    end
+    
+            
+endmodule
+
