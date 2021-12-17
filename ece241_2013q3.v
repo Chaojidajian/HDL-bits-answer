d19853b21e606ca1fcc38645f7a843c88a697705
Ricardo Menotti <menotti@gmail.com>
2021-12-17 14:33:49 -0300

    Update ece241_2013q3.v
    Fixing the state variables/parameters to the right width

--- ece241_2013q3.v
+++ ece241_2013q3.v
@@ -8,8 +8,8 @@ module top_module (
     output fr1,
     output dfr
 ); 
-    reg state,next_state;
-    parameter [3:0]A=0,B=1,C=2,D=3,E=4,F=5;
+	reg [2:0]state,next_state;
+	parameter [2:0]A=0,B=1,C=2,D=3,E=4,F=5;
     always@(*)begin
         case(state)
             A:if(s==3'b001)begin
